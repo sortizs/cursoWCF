168b0871109565797fc4288cda8a062d7fe5e596
Sebastian Ortiz Serna <sortizs@eafit.edu.co>
2019-05-15 17:22:54 -0500

    First commit

---
+++ SOAP/Pacagroup.Comercial.Credits.Domain/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Pacagroup.Comercial.Credits.Domain
+{
+    [DataContract]
+    public class Client
+    {
+        [DataMember]
+        public int IdClient { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Lastname { get; set; }
+        [DataMember]
+        public string SecondLastname { get; set; }
+        [DataMember]
+        public string DocumentType { get; set; }
+        [DataMember]
+        public string DocumentNumber { get; set; }
+        [DataMember]
+        public string Gender { get; set; }
+        [DataMember]
+        public DateTime BornDate { get; set; }
+        [DataMember]
+        public string Address { get; set; }
+        [DataMember]
+        public string PostalCode { get; set; }
+        [DataMember]
+        public string CivilStatus { get; set; }
+    }
+}
---
+++ SOAP/Pacagroup.Comercial.Credits.Domain/Credit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Pacagroup.Comercial.Credits.Domain
+{
+    [DataContract]
+    class Credit
+    {
+        [DataMember]
+        public int IdCredit { get; set; }
+        [DataMember]
+        public int CreditType { get; set; }
+        [DataMember]
+        public int IdClient { get; set; }
+        [DataMember]
+        public DateTime CreditDate { get; set; }
+        [DataMember]
+        public decimal Amount { get; set; }
+        [DataMember]
+        public DateTime PaymentDate { get; set; }
+        [DataMember]
+        public decimal Rate { get; set; }
+        [DataMember]
+        public decimal Commission { get; set; }
+    }
+}
